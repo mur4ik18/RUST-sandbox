2ed6cf2d28fa0c1f53a65bbef23aaa7ceffeaa83
mur4ik18 <o.o.kotov1999@gmail.com>
2022-03-24 22:12:41 +0100

    :rocet: init

---
+++ weather_app/src/main.rs
@@ -0,0 +1,22 @@
+extern crate reqwest;
+
+fn main() {
+    let api_key: &str = "9cb6de7ec25ee5dc276df1539830eb35";
+    // let mut easy = Easy::new();
+    let url = format!("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid={}", api_key);
+    let mut data: [1, 1] =
+    match reqwest::get(&url) {
+        Ok(mut response) => {
+            // Check if 200
+            if response.status() == reqwest::StatusCode::Ok {
+                match response.text(){
+                    Ok(text) => println!("Response text = {}", text),
+                    Err(_) => println!("no"),
+                }
+            } else {
+                println!("Response code not 200");
+            }
+        },
+        Err(err) => println!("Somthing wrong!! : {}", err),
+    }
+}
